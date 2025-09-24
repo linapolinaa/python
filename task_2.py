4fa96bfa31cf35697f525180e73c560ad9d0a2a7
linapolinaa <1polinaduybanova@mail.ru>
2025-09-25 00:17:31 +0300

    new2

--- task_2.py
+++ task_2.py
@@ -1,4 +1,4 @@
-s = input()
-for char in "aeiou":
-    s = s.replace(char, "")
-print(s)
+
+text=input("введите строку:")
+new_text=text.replace('a','').replace('e','').replace('i','').replace('o','').replace('u','').replace('A','').replace('E','').replace('O','').replace('I','').replace('U','')
+print(new_text)
