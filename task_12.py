12d949ec0f342c601745ce569476450b17253946
linapolinaa <1polinaduybanova@mail.ru>
2025-09-25 00:44:19 +0300

    new12

--- task_12.py
+++ task_12.py
@@ -1,47 +1,27 @@
-base_price = 24.99
-base_minutes = 60
-base_sms = 30
-base_internet = 1024  
+print("Тарифный план: 60 мин. разговора, 30 смс, 1ГБ трафика\nЦена 24,99 руб. в месяц")
+print("Каждая дополнительная минута стоит 0,89 руб., каждое дополнительное сообщение –0,59 руб., каждый дополнительный Мб – 0,79 руб.")
 
-minute_price = 0.89
-sms_price = 0.59
-internet_price = 0.79
+minutes=int(input("Введите количество израсходованных за месяц минут разговора: "))
+sms=int(input("Введите количество израсходованных за месяц смс: "))
+traffic=int(input("Введите количество израсходованного за месяц трафика в МБ: "))
+print('-'*40)
+if minutes>60:
+    sumDopMin=(minutes-60)*0.89
+    print(f"Доп.услуги за минуты разговора: {sumDopMin:.2f} руб.")
+else: sumDopMin=0
+if sms>30:
+    sumDopSms=(sms-30)*0.59
+    print(f"Доп.услуги за смс: {sumDopSms:.2f} руб.")
+else: sumDopSms=0
+if traffic>1000:
+    sumDopTraffic=(traffic-1000)*0.79
+    print(f"Доп.услуги за трафик: {sumDopTraffic:.2f} руб.")
+else: sumDopTraffic=0
 
-used_minutes = int(input("Минуты: "))
-used_sms = int(input("смс: "))
-used_internet = int(input("МБ интернета: "))
+if minutes<60 and sms<30 and traffic<1000:
+    print("Общая сумма: 24,99 руб.")
 
-extra_minutes = used_minutes - base_minutes
-extra_sms = used_sms - base_sms
-extra_internet = used_internet - base_internet
-
-if extra_minutes < 0:
-    extra_minutes = 0
-if extra_sms < 0:
-    extra_sms = 0
-if extra_internet < 0:
-    extra_internet = 0
-
-extra_minutes_cost = extra_minutes * minute_price
-extra_sms_cost = extra_sms * sms_price
-extra_internet_cost = extra_internet * internet_price
-
-total_no_tax = base_price + extra_minutes_cost + extra_sms_cost + extra_internet_cost
-
-tax = total_no_tax * 0.02
-
-total = total_no_tax + tax
-
-print(f"\nБазовя стоимость: {base_price:.2f} руб.")
-
-if extra_minutes > 0:
-    print(f"Доп. минуты: {extra_minutes_cost:.2f} руб.")
-
-if extra_sms > 0:
-    print(f"Доп. СМС: {extra_sms_cost:.2f} руб.")
-
-if extra_internet > 0:
-    print(f"Доп. интернет: {extra_internet_cost:.2f} руб.")
-
-print(f"Налог: {tax:.2f} руб.")
-print(f"Итого: {total:.2f} руб.")
+if minutes>60 or sms>30 or traffic>1000:
+    totalSum=24.99+sumDopMin+sumDopSms+sumDopTraffic+(24.99+sumDopMin+sumDopSms+sumDopTraffic)*0.02
+    print(f"Общая сумма: {totalSum:.2f}")
+print('-'*40)
