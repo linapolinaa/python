90b3c83ae57c7537ee9b53c22edd6e87bd084fb0
linapolinaa <1polinaduybanova@mail.ru>
2025-09-27 14:01:17 +0300

    измененная9

--- task_9.py
+++ task_9.py
@@ -1,14 +1,14 @@
 print("введите IP адрес в формате (XXX.XXX.XXX.XXX, XXX-число от 0 дo 255)")
-string=input()
-if string[3] != '.' or string[7] != '.' or string[11] != '.':
-        print("it is not a valid IP address")
-if int(string[0:3])>255 or int(string[0:3]) < 0:
+str=input()
+if str[3] != '.' or str[7] != '.' or str[11] != '.':
+        print("Некорректный IP адрес")
+if int(str[0:3])>255 or int(str[0:3]) < 0:
     print("это не IP адрес")
-elif int(string[4:6])>255 or int(string[4:7]) < 0:
+elif int(str[4:6])>255 or int(str[4:7]) < 0:
     print("это не IP адрес")
-elif int(string[8:11])>255 or int(string[8:11]) < 0:
+elif int(str[8:11])>255 or int(str[8:11]) < 0:
     print("это не IP адрес")
-elif int(string[12:15])>255 or int(string[12:15]) < 0:
+elif int(str[12:15])>255 or int(str[12:15]) < 0:
     print("это не IP адрес")
 else:
     print("это IP адрес")
