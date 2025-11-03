2b1a758b0c6fc6494eadf9e65a64f9fca26026f7
linapolinaa <1polinaduybanova@mail.ru>
2025-11-04 01:25:06 +0300

    изменения1

--- task2.2_1.py
+++ task2.2_1.py
@@ -2,41 +2,73 @@ def flatten_list(lst):
    
     i = 0
     while i < len(lst):
-        if type(lst[i]) == list:
+        if isinstance(lst[i], list):
+
             flatten_list(lst[i])
-            
             inner_list = lst[i]
             del lst[i]
+          
             for j in range(len(inner_list)):
                 lst.insert(i + j, inner_list[j])
         else:
             i += 1
 
 
-if __name__ == "__main__":
-    print("Программа преобразует списки с вложенностью в плоские списки")
-    print()
-    print("Примеры ввода:")
-    print("- [1, 2, [3, 4]]")
-    print()
+def parse_list_input(input_string):
+   
+    result = []
+    current_number = ""
+    i = 0
     
-    user_input = input("Введите ваш список: ")
-    
-    try:
-        user_list = eval(user_input)
+    while i < len(input_string):
+        char = input_string[i]
         
-        if type(user_list) != list:
-            print("Ошибка: введенные данные не являются списком!")
+        if char == '[':
+           
+            bracket_count = 1
+            j = i + 1
+            while j < len(input_string) and bracket_count > 0:
+                if input_string[j] == '[':
+                    bracket_count += 1
+                elif input_string[j] == ']':
+                    bracket_count -= 1
+                j += 1
+            
+            
+            inner_list = parse_list_input(input_string[i+1:j-1])
+            result.append(inner_list)
+            i = j
+            
+        elif char.isdigit():
+            current_number += char
+            i += 1
+            
+        elif char in ', ' and current_number:
+            result.append(int(current_number))
+            current_number = ""
+            i += 1
         else:
-            print()
-            print(f"Исходный список: {user_list}")
-           
-            original_id = id(user_list)
-           
-            flatten_list(user_list)
-            
-            print(f"Результат: {user_list}")
-            
-    except:
-        print("неверный формат списка")
+            i += 1
+    
+   
+    if current_number:
+        result.append(int(current_number))
+    
+    return result
+
+
+if __name__ == "__main__":
+    print("Программа преобразует вложенные списки в плоские")
+    print("Пример ввода: [1, 2, [3, 4]]")
+    print()
+    
+    user_input = input("Введите список: ").strip()
+    
+    user_list = parse_list_input(user_input)
+    
+    print(f"Исходный список: {user_list}")
+    
+    flatten_list(user_list)
+    
+    print(f"Результат: {user_list}")
        
