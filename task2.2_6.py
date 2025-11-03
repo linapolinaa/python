a042cf4732e06aa12192d90d9e554178c7af65e8
linapolinaa <1polinaduybanova@mail.ru>
2025-11-04 01:36:42 +0300

    изменения6

--- task2.2_6.py
+++ task2.2_6.py
@@ -13,24 +13,57 @@ def unique_elements(nested_list):
     return result
 
 
+def parse_list_input(input_string):
+   
+    result = []
+    current_number = ""
+    i = 0
+    
+    while i < len(input_string):
+        char = input_string[i]
+        
+        if char == '[':
+            
+            bracket_count = 1
+            j = i + 1
+            while j < len(input_string) and bracket_count > 0:
+                if input_string[j] == '[':
+                    bracket_count += 1
+                elif input_string[j] == ']':
+                    bracket_count -= 1
+                j += 1
+           
+            inner_list = parse_list_input(input_string[i+1:j-1])
+            result.append(inner_list)
+            i = j
+            
+        elif char.isdigit():
+            current_number += char
+            i += 1
+            
+        elif char in ', ' and current_number:
+            result.append(int(current_number))
+            current_number = ""
+            i += 1
+        else:
+            i += 1
+   
+    if current_number:
+        result.append(int(current_number))
+    
+    return result
+
+
 if __name__ == "__main__":
     print("поиск уникальных элементов во вложенном списке")
     print("Пример: [1, 2, 3, [4, 5], 6, [7, [8, 9]]]")
     print()
     
-    while True:
-        user_input = input("Введите список ")
-        
-        try:
-            user_list = eval(user_input)
-            if not isinstance(user_list, list):
-                print("введите корректный список")
-                continue
-        except:
-            print("введите корректный список")
-            continue
-        
-        unique = unique_elements(user_list)
-        
-        print(f"Уникальные элементы: {unique}")
-        
+    user_input = input("Введите список: ")
+    
+    # Преобразуем ввод в список без eval
+    user_list = parse_list_input(user_input)
+    
+    unique = unique_elements(user_list)
+    
+    print(f"Уникальные элементы: {unique}")
